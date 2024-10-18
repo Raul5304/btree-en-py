e04943e48d60e10593df9a605e0c8ab269b798ea
Raul5304 <raullecinena@iesrioarba.es>
2024-10-18 14:09:23 +0200

    first commit

---
+++ bnodo.py
@@ -0,0 +1,17 @@
+#! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+class bnodo:
+
+    __info = None
+    __de = None
+    __iz = None
+
+    def __init__(self, info = None):
+        self.__info = info
+        
+    def __str__(self):
+        answ = ""
+
+if __name__ == "__main__":
+
+    bn0 = bnodo()
